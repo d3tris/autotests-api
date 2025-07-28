062498212af0fd91179526b6f82f47da44c33355
d3tris <daurov97@gmail.com>
2025-07-28 13:15:07 +0200

    Introduction to the gRPC protocol

---
+++ grpc_server.py
@@ -0,0 +1,23 @@
+import grpc
+from concurrent import futures
+import user_service_pb2
+import user_service_pb2_grpc
+
+
+class UserServiceServicer(user_service_pb2_grpc.UserServiceServicer):
+    def GetUser(self, request, context):
+        print(f'A request was received for the GetUser method from a user: {request.username}')
+        return user_service_pb2.GetUserResponse(message=f"Hi, {request.username}!")
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    user_service_pb2_grpc.add_UserServiceServicer_to_server(UserServiceServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("gRPC server is running on port 50051...")
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
