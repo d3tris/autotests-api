0d39aa0137d6d13025fa8a605e4402c799e0b699
d3tris <daurov97@gmail.com>
2025-07-31 09:44:29 +0200

    Practice with TCP/IP protocol UPD

--- tcp_users_server.py
+++ tcp_users_server.py
@@ -1,7 +1,5 @@
 import socket
 
-messages = []
-
 
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,18 +9,16 @@ def server():
 
     server_socket.listen(10)
     print("Сервер запущен и ожидает подключений...")
-
+    messages: list[str] = []
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Пользователь с адресом: {client_address} подключился к серверу")
 
-        data = client_socket.recv(1024).decode()
-        if data:
-            print(f"Пользователь с адресом: {client_address} отправил сообщение: {data}")
-            messages.append(data)
+        message = client_socket.recv(1024).decode()
+        print(f"Пользователь с адресом: {client_address} отправил сообщение: {message}")
+        messages.append(message)
 
-            client_socket.send('\n'.join(messages).encode())
-
+        client_socket.send('\n'.join(messages).encode())
         client_socket.close()
 
 
