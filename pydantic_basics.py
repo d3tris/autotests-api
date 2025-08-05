425f77c108d002b867d20b8e3e6894d920740381
d3tris <daurov97@gmail.com>
2025-08-05 23:26:16 +0200

    Practice developing Pydantic models

--- pydantic_basics.py
+++ pydantic_basics.py
@@ -31,13 +31,10 @@ class CourseSchema(BaseModel):
 
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     title: str = "Playwright"
-    # max_score: int
-    # min_score: int
     max_score: int = Field(alias='maxScore', default=1000)
     min_score: int = Field(alias='minScore', default=100)
     description: str = "Playwright course"
     preview_file: FileSchema = Field(alias='previewFile')
-    # estimated_time: str
     estimated_time: str = Field(alias='estimatedTime', default="2 weeks")
     created_by_user: UserSchema = Field(alias='createdByUser')
 
