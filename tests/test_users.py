683883553166d4148c51de2f746d705f43e14cec
d3tris <daurov97@gmail.com>
2025-08-08 09:51:12 +0200

    Pytest Markers

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 from http import HTTPStatus
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -5,7 +6,8 @@ from tools.assertions.users import assert_create_user_response
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
-
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_users_client = get_public_users_client()
 
