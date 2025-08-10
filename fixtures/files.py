93d015d1b0fb8e69ae9c1021cb781b1e4987b0ea
d3tris <daurov97@gmail.com>
2025-08-10 13:59:45 +0200

    Refactoring and preparation for developing API autotests

---
+++ fixtures/files.py
@@ -0,0 +1,21 @@
+import pytest
+from pydantic import BaseModel
+from fixtures.users import UserFixture
+from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
+from clients.files.files_client import FilesClient, get_files_client
+
+
+class FileFixture(BaseModel):
+    request: CreateFileRequestSchema
+    response: CreateFileResponseSchema
+
+
+@pytest.fixture
+def files_client(function_user: UserFixture) -> FilesClient:
+    return get_files_client(function_user.authentication_user)
+
+
+def function_file(files_client: FilesClient) -> FileFixture:
+    request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+    response = files_client.create_file(request)
+    return FileFixture(request=request, response=response)
