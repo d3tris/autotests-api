aa2de9a185c2c1ed2a9d0d373ed5c40f5916eaa9
d3tris <daurov97@gmail.com>
2025-08-15 16:29:12 +0200

    Practice of writing API autotests for working with courses

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,12 +1,15 @@
 import pytest
 from http import HTTPStatus
+
+from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 from fixtures.courses import CourseFixture
 from tools.assertions.base import assert_status_code
 from clients.courses.courses_client import CoursesClient
 from clients.courses.courses_schema import UpdateCourseRequestSchema, UpdateCourseResponseSchema, GetCoursesQuerySchema, \
-    GetCoursesResponseSchema
-from tools.assertions.courses import assert_update_course_response, assert_get_courses_response
+    GetCoursesResponseSchema, CreateCourseRequestSchema, CreateCourseResponseSchema
+from tools.assertions.courses import assert_update_course_response, assert_get_courses_response, \
+    assert_create_course_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -35,5 +38,23 @@ class TestCourses:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_update_course_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_create_course(
+            self,
+            courses_client: CoursesClient,
+            function_file: FileFixture,
+            function_user: UserFixture
+    ):
+        request = CreateCourseRequestSchema(
+            preview_file_id=function_file.response.file.id,
+            created_by_user_id=function_user.response.user.id
+        )
+        response = courses_client.create_course_api(request)
+        response_data = CreateCourseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_course_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
