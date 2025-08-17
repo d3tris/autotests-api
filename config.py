f3378ed412e1311fea9198df476b32945606227b
d3tris <daurov97@gmail.com>
2025-08-17 17:14:57 +0200

    Adding environment variables information to the Allure report

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, HttpUrl, FilePath
+from typing import Self
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, HttpUrl, FilePath, DirectoryPath
 
 
 class HTTPClientConfig(BaseSettings):
@@ -24,6 +25,14 @@ class Settings(BaseSettings):
 
     test_data: TestDataConfig
     http_client: HTTPClientConfig
+    allure_results_dir: DirectoryPath
+
+    @classmethod
+    def initialize(cls) -> Self:  # Возвращает экземпляр класса Settings
+        allure_results_dir = DirectoryPath("./allure-results")  # Создаем объект пути к папке
+        allure_results_dir.mkdir(exist_ok=True)  # Создаем папку allure-results, если она не существует
+
+        return Settings(allure_results_dir=allure_results_dir)  # Передаем allure_results_dir в инициализацию настроек
 
 
-settings = Settings()
+settings = Settings.initialize()
--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 pytest_plugins = (
     "fixtures.users",
     "fixtures.files",
+    "fixtures.allure",
     "fixtures.courses",
     "fixtures.exercises",
     "fixtures.authentication"
